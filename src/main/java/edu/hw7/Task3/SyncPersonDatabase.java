29a296126a63fc291edf300569e3c44747d1f8c5
Ovoshchko <ashinyakov@bk.ru>
2023-12-01 00:19:47 +0300

    Fixed

--- src/main/java/edu/hw7/Task3/SyncPersonDatabase.java
+++ src/main/java/edu/hw7/Task3/SyncPersonDatabase.java
@@ -1,68 +1,33 @@
 package edu.hw7.Task3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SyncPersonDatabase implements PersonDatabase {
 
-    private final Map<Integer, Person> personDatabase = new HashMap<>();
-    private final Map<String, List<Person>> personsByName = new HashMap<>();
-    private final Map<String, List<Person>> personsByAddress = new HashMap<>();
-    private final Map<String, List<Person>> personsByPhone = new HashMap<>();
+    private final StandartPersonDatabase database = new StandartPersonDatabase();
 
     @Override
     public synchronized void add(Person person) {
-
-        if (personDatabase.containsKey(person.id())) {
-            delete(person.id());
-        }
-
-        personDatabase.put(person.id(), person);
-        addIndex(personsByName, person.name(), person);
-        addIndex(personsByAddress, person.address(), person);
-        addIndex(personsByPhone, person.phoneNumber(), person);
+        database.add(person);
     }
 
     @Override
     public synchronized void delete(int id) {
-        Person person = personDatabase.remove(id);
-
-        if (person != null) {
-            deleteIndex(personsByName, person.name(), person);
-            deleteIndex(personsByAddress, person.address(), person);
-            deleteIndex(personsByPhone, person.phoneNumber(), person);
-        }
+        database.delete(id);
     }
 
     @Override
     public synchronized List<Person> findByName(String name) {
-        return personsByName.getOrDefault(name, new ArrayList<>());
+        return database.findByName(name);
     }
 
     @Override
     public synchronized List<Person> findByAddress(String address) {
-        return personsByAddress.getOrDefault(address, new ArrayList<>());
+        return database.findByAddress(address);
     }
 
     @Override
     public synchronized List<Person> findByPhone(String phone) {
-        return personsByPhone.getOrDefault(phone, new ArrayList<>());
-    }
-
-    private boolean addIndex(Map<String, List<Person>> indexList, String indexParameter, Person person) {
-        return indexList.computeIfAbsent(indexParameter, p -> new ArrayList<>()).add(person);
-    }
-
-    private void deleteIndex(Map<String, List<Person>> indexList, String indexParameter, Person person) {
-        List<Person> people = indexList.get(indexParameter);
-
-        if (people != null) {
-            people.remove(person);
-            if (people.isEmpty()) {
-                indexList.remove(indexParameter);
-            }
-        }
+        return database.findByPhone(phone);
     }
 }
